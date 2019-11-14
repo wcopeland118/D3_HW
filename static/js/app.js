019b35f7d8036f67b5d86f3f29884656c05043a9
wcopeland118 <wcopeland118@gmail.com>
2019-11-14 12:41:44 -0600

    create file structure and added code from exercise

---
+++ static/js/app.js
@@ -0,0 +1,12 @@
+var svgWidth = 960;
+var svgHeight = 500;
+
+var margin = {
+  top: 20,
+  right: 40,
+  bottom: 80,
+  left: 100
+};
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
